59e8d50da18ac908aac14049d1ae6de65f4e556f
raultr <raultr@gmail.com>
2018-03-06 21:32:37 -0600

    Se agrego el primer modelo en la bd

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,10 @@
 from django.urls import resolve
 from django.test import TestCase
-
+from lists.models import Item
 from lists.views import home_page
 
-
 class HomPageTest(TestCase):
+
     def test_root_utl_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -26,3 +26,22 @@ class HomPageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
