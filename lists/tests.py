3cd08a04491890d520190bca5c82590f6333e972
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2018-03-07 16:18:59 -0600

    Se crearon las rutas por separado para consultar e insertar

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from lists.models import Item
 from lists.views import home_page, view_list
 
-class HomPageTest(TestCase):
+class NewListTest(TestCase):
 
     def test_root_utl_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -27,15 +27,16 @@ class HomPageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
