0fd018cf76a9d69c632a7bb78589276295537314
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2018-03-12 13:30:42 -0600

    verificacion de la variable para saber el area de desarrollo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
 import time
 
 MAX_WAIT = 10
@@ -11,6 +12,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
 
     def tearDown(self):
         self.browser.refresh()
@@ -141,6 +146,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            2,
+            266,
             delta=10
         )
