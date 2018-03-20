3c3497e5a6183e9b3a1f1e6c4e4334bcd0cde3f5
raultr <raultr@gmail.com>
2018-03-19 21:03:47 -0600

    ruta requeriments estaba mal

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -30,7 +30,7 @@ def _get_latest_source():
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
         run(f'python3.6 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+    run('./virtualenv/bin/pip install -r requeriments.txt')
 
 
 def _create_or_update_dotenv():
