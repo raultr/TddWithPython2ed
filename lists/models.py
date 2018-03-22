f76a9aa52239c4ab59a157b578467acc2d0cf7f6
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2018-03-22 13:23:09 -0600

    nueva plantilla de python para formularios genericos

--- lists/models.py
+++ lists/models.py
@@ -6,6 +6,7 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
+
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete= models.PROTECT)
+    list = models.ForeignKey(List, default=None, on_delete=models.PROTECT)
