1cab879a759d3ab605fc28cc1409a87935f3feb2
raultr <raultr@gmail.com>
2018-04-16 16:17:49 -0500

    loggin en settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -114,6 +114,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
